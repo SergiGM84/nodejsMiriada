1fcf59e8892cbf6faf326e901e634b76d2cffb12
Sergi Gutierrez <sergi.gutierrez@freematica.com>
2015-07-07 09:39:19 +0200

    adaptive layout view

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+// GET Question
+exports.question = function(req, res){
+    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+// GET Answer
+exports.answer = function(req, res){
+    if(req.query.respuesta === 'Roma'){
+        res.render('quizes/answer', {respuesta: 'Correcto'});
+    } else{
+        res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+};
