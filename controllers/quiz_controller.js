08c9b3ee395439cae3af868da4c1eec936e6448d
Sergi Gutierrez <sergi.gutierrez@freematica.com>
2015-07-07 13:23:47 +0200

    add sqlite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,21 @@
+var models = require('../models/models.js');
+
 // GET Question
 exports.question = function(req, res){
-    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+    models.Quiz.findAll().success(function(quiz){
+        console.log('quiz',quiz);
+        res.render('quizes/question', {pregunta: quiz[0].pregunta});
+    });
 };
 
 // GET Answer
 exports.answer = function(req, res){
-    if(req.query.respuesta === 'Roma'){
-        res.render('quizes/answer', {respuesta: 'Correcto'});
-    } else{
-        res.render('quizes/answer', {respuesta: 'Incorrecto'});
-    }
+    models.Quiz.findAll().success(function(quiz){
+        if(req.query.respuesta === quiz[0].respuesta){
+            res.render('quizes/answer', {respuesta: 'Correcto'});
+        } else{
+            res.render('quizes/answer', {respuesta: 'Incorrecto'});
+        }
+    });
+
 };
---
+++ models/quiz.js
@@ -0,0 +1,14 @@
+// Definicion del modelo de Quiz con validación
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+    'Quiz',
+    { pregunta: {
+        type: DataTypes.STRING
+      },
+      respuesta: {
+        type: DataTypes.STRING
+      }
+    }
+  );
+}
