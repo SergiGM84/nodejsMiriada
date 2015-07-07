22dbd39a84345df8d57a8557f94490e0b82151ef
Sergi Gutierrez <sergi.gutierrez@freematica.com>
2015-07-07 17:26:56 +0200

    autoload

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
-/* GET home page. */
+/* GET / */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-/* GET autor page. */
+// autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+/* GET autor/ */
 router.get('/autor', function(req, res) {
   res.render('autor');
 });
 
-/* GET quizes questions. */
-router.get('/quizes/question', quizController.question);
-/* GET quizes answer. */
-router.get('/quizes/answer', quizController.answer);
+/* GET quizes/ */
+router.get('/quizes', quizController.index);
+/* GET quizes/:id */
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+/* GET quizes/:id/answer. */
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
